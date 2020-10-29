99960651bf8b85977dfadb2b63b54832bf4f1c27
Elbert Wang <elbert.wang@wsj.com>
2020-10-29 00:58:21 -0400

    cleaning up code and centerign text boxes

--- script.js
+++ script.js
@@ -1,80 +1,7 @@
-var main = d3.select("main");
-var scrolly = main.select("#scrolly");
-var figure = scrolly.select("figure");
-var article = scrolly.select("article");
-var step = article.selectAll(".step");
-
 var scroller = scrollama();
-
-
-figure.append('img').attr('class', 'step-0').attr('opacity', 0).attr("src", "img/" + "step-0" + ".png")
-
-
-figure.append('img').attr('class', 'step-1').attr('opacity', 0)
-  .attr("src", "img/" + "step-1" + ".gif")
-
-
-figure.append('img').attr('class', 'step-2').attr('opacity', 0)
-  .attr("src", "img/" + "step-2" + ".gif")
-
-
-figure.append('img').attr('class', 'step-3').attr('opacity', 0)
-  .attr("src", "img/" + "step-3" + ".gif")
-
-
-figure.append('img').attr('class', 'step-4').attr('opacity', 0)
-  .attr("src", "img/" + "step-4" + ".png")
-
-figure.append('img').attr('class', 'step-5').attr('opacity', 0)
-  .attr("src", "img/" + "step-5" + ".png")
-
-figure.append('img').attr('class', 'step-6').attr('opacity', 0)
-  .attr("src", "img/" + "step-6" + ".gif")
-
-figure.append('img').attr('class', 'step-7').attr('opacity', 0).attr("src", "img/" + "step-7" + ".gif")
-
-figure.append('img').attr('class', 'step-8').attr('opacity', 0).attr("src", "img/" + "step-8" + ".jpg")
-
-figure.append('img').attr('class', 'step-9').attr('opacity', 0).attr("src", "img/" + "step-9" + ".gif")
-figure.append('img').attr('class', 'step-10').attr('opacity', 0).attr("src", "img/" + "step-10" + ".gif")
-figure.append('img').attr('class', 'step-11').attr('opacity', 0).attr("src", "img/" + "step-11" + ".gif")
-
-figure.append('img').attr('class', 'step-12').attr('opacity', 0).attr("src", "img/" + "step-12" + ".gif")
-
-figure.append('img').attr('class', 'step-13').attr('opacity', 0).attr("src", "img/" + "step-13" + ".png")
-
-figure.append('img').attr('class', 'step-14').attr('opacity', 0).attr("src", "img/" + "step-14" + ".gif")
-
-figure.append('img').attr('class', 'step-15').attr('opacity', 0).attr("src", "img/" + "step-15" + ".gif")
-
-figure.append('img').attr('class', 'step-16').attr('opacity', 0).attr("src", "img/" + "step-16" + ".gif")
-
-figure.append('img').attr('class', 'step-17').attr('opacity', 0).attr("src", "img/" + "step-17" + ".png")
-
-figure.append('img').attr('class', 'step-18').attr('opacity', 0).attr("src", "img/" + "step-18" + ".png")
-
-figure.append('img').attr('class', 'step-19').attr('opacity', 0).attr("src", "img/" + "step-19" + ".png")
-
-figure.append('img').attr('class', 'step-20').attr('opacity', 0).attr("src", "img/" + "step-20" + ".png")
-
-figure.append('img').attr('class', 'step-21').attr('opacity', 0).attr("src", "img/" + "step-21" + ".gif")
-
 
 // generic window resize listener event
 function handleResize() {
-  // 1. update height of step elements
-  var stepH = Math.floor(window.innerHeight * 0.75);
-  step.style("height", stepH + "px");
-
-  var figureHeight = window.innerHeight;
-
-  var figureMarginTop = 0;
-
-
-  figure
-    .style("height", figureHeight + "px")
-    .style("top", figureMarginTop + "px");
-
   // 3. tell scrollama to update new element dimensions
   scroller.resize();
 }
@@ -82,46 +9,30 @@ function handleResize() {
 // scrollama event handlers
 function handleStepEnter(response) {
   console.log(response);
-  console.log(response.index, '-------- enter', response.direction);
+  console.log(response.index, "-------- enter", response.direction);
   // response = { element, direction, index }
-  response.element.classList.add('is-active');
-
-
-
-  if (response.direction == 'down') figure.select('.step-' + response.index).style('opacity', 0).transition().duration(1000).ease(d3.easeLinear).style("opacity", 1);
-
-
+  response.element.classList.add("is-active");
+  document
+    .getElementsByClassName(`img-step-${response.index}`)[0]
+    .classList.add("is-active");
 }
 
 function handleStepExit(response) {
   // response = { element, direction, index }
-  console.log(response.index, '-------- exit', response.direction);
+  console.log(response.index, "-------- exit", response.direction);
   // remove color from current step
-  response.element.classList.remove('is-active');
-  // hide corresponding map step if scrolling up
-  if (response.direction == 'up')
-    figure.select('.step-' + (response.index)).style('opacity', 1).transition().duration(1000).ease(d3.easeLinear).style('opacity', 0);
-
-
-
+  response.element.classList.remove("is-active");
+  document
+    .getElementsByClassName(`img-step-${response.index}`)[0]
+    .classList.remove("is-active");
 }
 
 function handleStepProgress(response) {
   // response = { element, progress, index }
-  console.log(response.index, '-------- progress -', response.progress);
-
-
-}
-
-function setupStickyfill() {
-  d3.selectAll(".sticky").each(function() {
-    Stickyfill.add(this);
-  });
+  console.log(response.index, "-------- progress -", response.progress);
 }
 
 function init() {
-  setupStickyfill();
-
   // 1. force a resize on load to ensure proper dimensions are sent to scrollama
   handleResize();
 
@@ -129,12 +40,11 @@ function init() {
   // 		this will also initialize trigger observations
   // 3. bind scrollama event handlers (this can be chained like below)
 
-
   scroller
     .setup({
-      step: "#scrolly article .step",
+      step: "#scrolly .scroll-scenes .step",
       offset: 1,
-      debug: true
+      debug: true,
     })
     .onStepEnter(handleStepEnter)
     .onStepExit(handleStepExit)
